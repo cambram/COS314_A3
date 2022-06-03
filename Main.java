b54b79c75bde1b57a87b7de991f6a59b0d153644
cambram <cambram@icloud.com>
2022-06-03 23:32:52 +0200

    Testing linked list

--- Main.java
+++ Main.java
@@ -1,5 +1,34 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String args[]) {
-        System.out.println("Hello World");
+        String data = "balance-scale.data";
+        String line = "";
+        BufferedReader reader = null;
+
+        NeuralNetwork NeuNet = new NeuralNetwork();
+        LinkedList<String[]> datapoints = new LinkedList<String[]>();
+
+        try {
+            reader = new BufferedReader(new FileReader(data));
+            while ((line = reader.readLine()) != null) {
+                String[] split = line.split(",");
+                datapoints.add(split);
+
+            }
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
     }
 }
